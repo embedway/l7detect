90c7455eca6bb07d86015dbdcd135ade3e1ed714
xdzhou <xdzhou@embedway.com>
2011-08-28 17:58:02 +0800

    去掉了application.mk中的cvmx

--- build/application.mk
+++ build/application.mk
@@ -1,5 +1,3 @@
-$(OBJ_DIR):
-	mkdir $(OBJ_DIR)
 $(DYNLIB_DIR):
 	mkdir $@
 #  applications object suffix rule
@@ -30,40 +28,12 @@ $(MADE_WITH):$(USER_CONF)
 
 $(TARGET) $(OBJS) $(CLEAN_LIST): $(MADE_WITH)
 
-#  application build target
 
-# The user decides if the build will create the cdb files for use with
-# the EDB debugger or the stripped binaries used to save space and
-# reduce download time by defining the EXTRA_CVMX_APPLICATION_TARGETS
-# environment variable.
-#
-# $ setenv EXTRA_CVMX_APPLICATION_TARGETS '$(TARGET).stp $(TARGET).cdb'
-#
-# If the variable is not defined the build works in the usual way.
-
-#   The user decides if the build will create archives by defining the
-#   CVMX_ARCHIVE_DIRECTORY environment variable.
-#   
-#   $ setenv CVMX_ARCHIVE_DIRECTORY $HOME/archive
-#   
-#   If the variable is not defined the build works in the usual way.
-#   Building will create a unique subdirectory for each OCTEON_MODEL
-#   configuration.
-
-ARCHIVE_FILES = $(TARGET) $(EXTRA_CVMX_APPLICATION_TARGETS)
-
-archive: $(ARCHIVE_FILES)
-	-@ if [ "${CVMX_ARCHIVE_DIRECTORY}" != "" ] ; then \
-		mkdir -p ${CVMX_ARCHIVE_DIRECTORY}/${OCTEON_MODEL} ; \
-		cp -f $(ARCHIVE_FILES) ${CVMX_ARCHIVE_DIRECTORY}/${OCTEON_MODEL}/ ; \
-		chmod -R a+r ${CVMX_ARCHIVE_DIRECTORY}/${OCTEON_MODEL} ; \
-	 fi ; \
-
-application-target: $(TARGET) $(EXTRA_CVMX_APPLICATION_TARGETS) archive $(DYNLIB)
+application-target: $(TARGET) $(DYNLIB)
 
 #$(TARGET).stp: $(TARGET)
 #	$(STRIP) -o $(TARGET).stp $(TARGET)
 
-$(TARGET): $(CVMX_CONFIG) $(OBJS) $(LIBS_LIST)
+$(TARGET): $(OBJS) $(LIBS_LIST)
 	$(CC) $(OBJS) $(LDFLAGS_PATH) $(LIBS_LIST) $(LDFLAGS_GLOBAL) -o $@
 
